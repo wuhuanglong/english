dc2b4295f5703e30afcbfb6d42dd765778243069
wuhuanglong <2657332467@qq.com>
2017-09-14 15:41:45 +0800

    di er ci

--- PycharmProjects/xin/shang.py
+++ PycharmProjects/xin/shang.py
@@ -1,1 +1,15 @@
 print ("上传测试")
+
+import requests
+def get_all_cookie():
+    r = requests.get("http://qgc.qq.com/314962432")
+    c = r.cookies
+    i = c.items()
+    for name, value in i :
+        print (name, value)
+def get_all_cookie2():
+    session = requests.Session()
+    response = session.get("http://qgc.qq.com/314962432")
+    print (session.cookies.get_dict())
+if  __name__  == "__main__":
+    get_all_cookie2()
