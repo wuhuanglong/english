fd1e852c7d0a9eabae3e048703b74e2e62144469
wuhuanglong <2657332467@qq.com>
2017-10-07 15:23:06 +0800

    成gong

--- PycharmProjects/maoyan/spider.py
+++ PycharmProjects/maoyan/spider.py
@@ -14,11 +14,21 @@ def parse_one_page(html):
                         +'.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
                         +'.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>',re.S)
     items = re.findall(patten,html)
-    print (items)
+    for item in items:
+        yield  {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time':  item[4].strip()[5:],
+            'score': item[5]+item[6]
+        }
 def main():
     url = "http://maoyan.com/board/4?"
     html = get_one_page(url)
-    parse_one_page(html)
+    for item in parse_one_page(html):
+        print (item)
+
 if  __name__ == "__main__":
     main()
 
